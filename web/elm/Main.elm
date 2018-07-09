9c2e36cd2969c8cc6aa9670a1c03e248510ce00b
Kelaiya <kelaiyarao1@gmail.com>
2018-07-09 01:04:39 -0400

    Changed some variables name

--- web/elm/Main.elm
+++ web/elm/Main.elm
@@ -1,203 +1,3 @@
---dmodule Main exposing (main)
---import Html exposing (..)
---import Json.Decode exposing (..)
---import Html.Events exposing (..)
---import Http 
-
---import Html exposing (Html, button, div, text)
---import Html.Events exposing (onClick)
-
-
-----main =
-----  Html.beginnerProgram { model = model, view = view, update = update }
-
-
----- MODEL
-
---type alias Ticket =
---    { id : Int
---    , date : String
---    , opponent : String
---    , time : String
---    , userId : Maybe Int
---    }
---type alias User =
---    { id : Int
---    , name : String
---    }
---type alias Model =
---    { tickets : List Ticket
---    , users : List User
---    , myUserName : String
---    , systemError : String
---    }
-
---init : ( Model, Cmd Msg )
---init =
---    { tickets = []
---    , users = []
---    , myUserName = "Bill"
---    , systemError = ""
---    }
---        ! [ ticketsRequest, usersRequest ]
-
---{--Decoders--}
-
---ticketListDecoder : Json.Decode.Decoder (List Ticket)
---ticketListDecoder =
---    Json.Decode.list ticketDecoder
---ticketDecoder : Json.Decode.Decoder Ticket
---ticketDecoder =
---    Json.Decode.Pipeline.decode Ticket
---        |> Json.Decode.Pipeline.required "id" Json.Decode.int
---        |> Json.Decode.Pipeline.required "date" Json.Decode.string
---        |> Json.Decode.Pipeline.required "opponent" 
---             Json.Decode.string
---        |> Json.Decode.Pipeline.required "time" Json.Decode.string
---        |> Json.Decode.Pipeline.required "user_id" 
---            (Json.Decode.nullable Json.Decode.int)
---ticketsRequest : Cmd Msg
---ticketsRequest =
---    let
---        url =
---            "http://localhost:4000/api/v1/tickets"
---    in
---        Http.send ProcessTicketRequest
---          (Http.get url ticketListDecoder)
---userListDecoder : Json.Decode.Decoder (List User)
---userListDecoder =
---    Json.Decode.list userDecoder
---userDecoder : Json.Decode.Decoder User
---userDecoder =
---    Json.Decode.Pipeline.decode User
---        |> Json.Decode.Pipeline.required "id" Json.Decode.int
---        |> Json.Decode.Pipeline.required "name" Json.Decode.string
---usersRequest : Cmd Msg
---usersRequest =
---    let
---        url =
---            "http://localhost:4000/api/v1/users"
---    in
---        Http.send ProcessUserRequest (Http.get url userListDecoder)
----- UPDATE
---type Msg
---  = ProcessTicketRequest (Result Http.Error String)
---  | ProcessUserRequest (Result Http.Error String)
-
---update : Msg -> Model -> (Model, Cmd Msg)
---update msg model =
---  case msg of
---    ProcessTicketRequest (Ok tickets) ->
---        { model | tickets = tickets } ! []
---    ProcessTicketRequest (Err error) ->
---        let
---            errorString =
---                error |> toString |> String.slice 0 120
---        in
---            { model | systemError = errorString } ! []
---    ProcessUserRequest (Ok users) ->
---        { model | users = users } ! []
---    ProcessUserRequest (Err error) ->
---        let
---            errorString =
---                error |> toString |> String.slice 0 120
---        in
---            { model | systemError = errorString } ! []
-
-
----- VIEW
-
---view : Model -> Html Msg
---view model =
---    div []
---        [ header model.systemError
---        , remainingTickets model.tickets model.myUserName
---        , myTickets model
---        ]
-
---header : String -> Html Msg
---header systemError =
---    let
---        errorAttributes =
---            if model.systemError == "" then
---                []
---            else
---                []
---    in
---        div []
---            [ div errorAttributes [ text systemError ] ]
-
---remainingTickets : List Ticket -> Html Msg
---remainingTickets tickets myUserId =
---    div
---        []
---        [ h1 []
---            [ span
---                []
---                [ text "Remaining Tickets" ]
---            ]
---        , div []
---            (List.filter
---                (\ticket -> ticket.userId == Nothing)
---                    tickets
---                |> remainingTicketList
---            )
---        ]
---remainingTicketList : List Ticket -> List (Html Msg)
---remainingTicketList tickets =
---    List.map
---        (\ticket -> singleRemainingTicket ticket) tickets
---singleRemainingTicket : Ticket -> Html Msg
---singleRemainingTicket ticket =
---    let
---        innerDiv =
---            div []
---                [ div []
---                    [ div [] [ text ticket.date ]
---                    , div [] [ text ticket.opponent ]
---                    , div [] [ text ticket.time ]
---                    ]
---                ]
---    in
---        a [] [ innerDiv ]
-
---myTickets : Model -> Html Msg
---myTickets model =
---    let
---        myUserId = userIdFromName model.myUserName
---    in
---        div []
---            [ h1 []
---                [ span
---                    []
---                    [ text "My Tickets" ]
---                ]
---            , div []
---                (List.filter
---                    (\ticket -> ticket.userId == Just myUserId)
---                    model.tickets
---                    |> ticketList
---                )
---            ]
---userIdFromName : String -> List User -> Int
---userIdFromName name users =
---    List.filter (\user -> user.name == name) users
---        |> List.head
---        |> Maybe.withDefault nullUser
---        |> .id
-
-
-
---main : Program Never Model Msg
---main =
---  Html.program
---    { init = init 
---    , view = view
---    , update = update
---    , subscriptions = Cmd.none
---    }
-
-
 module Main exposing (..)
 
 import Html exposing (..)
@@ -206,8 +6,6 @@ import Html.Events exposing (..)
 import Http
 import Http exposing (Request)
 import Json.Decode as JD
-import Json.Encode as JE
-import Task exposing (..)
 
 
 {--Model--}
@@ -239,6 +37,7 @@ initModel =
    , error = ""
    }
 
+{-- Api --}
 
 api : String
 api =
@@ -282,10 +81,14 @@ getUser =
        (JD.field "name" JD.string)
        (JD.field "pic" JD.string)
 
+{-- Initial Command --}
+
 initialCmd : Cmd Msg
 initialCmd =
    Http.send GetDataFromBackend getBackendUsers
 
+
+{-- Msg --}
 
 type Msg
    = GetDataFromBackend (Result Http.Error (List Backend))
@@ -295,7 +98,6 @@ type Msg
 
 
 {--Update--}
-
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
@@ -315,12 +117,7 @@ update msg model =
             ( { model | error = toString error }, Cmd.none )
 
 
-
-
-
-
 {--View--}
-
 
 view : Model -> Html Msg
 view model =
@@ -353,6 +150,21 @@ view model =
             ]
        ]
 
+
+{-- Main --}
+
+main : Program Never Model Msg
+main =
+   program
+       { update = update
+       , view = view
+       , init = ( initModel, initialCmd )
+       , subscriptions = \_ -> Sub.none
+       }
+
+
+
+{-- Rough --}
 
 --postData : String -> Http.Body -> Http.Request String
 --postData ntitle =
@@ -414,11 +226,3 @@ view model =
 --empty 
 --    = Empty
 
-main : Program Never Model Msg
-main =
-   program
-       { update = update
-       , view = view
-       , init = ( initModel, initialCmd )
-       , subscriptions = \_ -> Sub.none
-       }
